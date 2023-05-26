472af7ba2a378905ebbab63de3066b56f2e37fda
LENV <technicalpriyanshutaru@gmail.com>
2023-05-26 23:22:44 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutterwayproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterwayproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutterwayproject/screens/splash/components/body.dart';
+import 'package:flutterwayproject/size_config.dart';
+
+
+
+class SplashScreen extends StatelessWidget {
+  static String routeName = "/splash";
+  @override
+  Widget build(BuildContext context) {
+    // You have to call it on your starting screen
+    SizeConfig().init(context);
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
